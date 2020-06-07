c0145d48ef632a456c5787cb10d7efdb42ff1325
xxKeefer <xxkeefer.code@gmail.com>
2020-06-07 21:41:45 +1000

    started test file for parser class

--- src/classes/ParseSudoku.rb
+++ src/classes/ParseSudoku.rb
@@ -1,10 +1,35 @@
+# frozen_string_literal: true
+# Sudoku String Parser
 class ParseSudoku
+  # Custom Errors
+  class NotStringError < StandardError
+    def message
+      'Input was not a string.'
+    end
+  end
+  class StringTooLong < StandardError
+    def message
+      'String is longer than 81 chracters.'
+    end
+  end
+  class StringTooShort < StandardError
+    def message
+      'String is shorter than 81 chracters.'
+    end
+  end
+  class StringHasInvalidCharacters < StandardError
+    def message
+      'String contains non numeric characters.'
+    end
+  end
+
+  # Class Methods
   def parse_sudoku(puzzle_string)
-    puzzle_string.insert(3,"|")
-    puzzle_string.insert(-4,"|")
-    puzzle_string.insert(0,"-")
-    puzzle_string.insert(-1,"-")
-    puzzle_string.split(//).join(' ')
-    
+    puzzle_string
+    # puzzle_string.insert(3,"|")
+    # puzzle_string.insert(-4,"|")
+    # puzzle_string.insert(0,"-")
+    # puzzle_string.insert(-1,"-")
+    # puzzle_string.split(//).join(' ')
   end
 end
--- src/test/test_solver.rb
+++ src/test/test_solver.rb
@@ -1,25 +1,42 @@
+# frozen_string_literal: true
+
 # given a text file
 
-
-=begin
-  if not string err
-
-  if string less than 81 chars return err
-
-  if string more than 81 chars return err
-
-  if string contains non numbers return err
-
-  else display formated string
-=end
+#   if not string err
+#
+#   if string less than 81 chars return err
+#
+#   if string more than 81 chars return err
+#
+#   if string contains non numbers return err
+#
+#   else display formated string
 
 require 'rspec/autorun'
 require_relative '../classes/ParseSudoku'
 
 describe ParseSudoku do
-  it 'recieves a valid string' do
+  before(:each) do
     puzzle = ParseSudoku.new
-
-    expect(puzzle.parse_sudoku("123456789")).to eq("- 1 2 3 | 4 5 6 | 7 8 9 -")
+  end
+  context 'Input is not a valid Sudoku String.' do
+    it 'input is nothing' do
+      expect(puzzle.parse_sudoku).to raise_error(NotStringError)
+    end
+    it 'input is nil' do
+      expect(puzzle.parse_sudoku(nil)).to raise_error(NotStringError)
+    end
+    it 'input is integer' do
+      expect(puzzle.parse_sudoku(123)).to raise_error(NotStringError)
+    end
+    it 'input is float' do
+      expect(puzzle.parse_sudoku(12.3)).to raise_error(NotStringError)
+    end
+    it 'input is boolean, true' do
+      expect(puzzle.parse_sudoku).to raise_error(NotStringError)
+    end
+    it 'input is boolean, false' do
+      expect(puzzle.parse_sudoku).to raise_error(NotStringError)
+    end
   end
 end
