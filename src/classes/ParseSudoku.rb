70037807af94407818bbf04c2b92c48d7ed17a33
xxKeefer <xxkeefer.code@gmail.com>
2020-06-08 01:04:58 +1000

    make ParseSudoku return formated grid given valid 81 char input.

--- src/classes/ParseSudoku.rb
+++ src/classes/ParseSudoku.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
+
 # Sudoku String Parser
 class ParseSudoku
+  require_relative '../mods/services.rb'
+  @format = Services.new
   # Custom Errors
   class NotStringError < StandardError
     def message
@@ -23,15 +26,36 @@ class ParseSudoku
     end
   end
 
-
-  
   def parse_sudoku(puzzle_string)
     raise NotStringError if puzzle_string.class != String
     raise StringTooLong if puzzle_string.length > 81
     raise StringTooShort if puzzle_string.length < 81
-    raise StringHasInvalidCharacters if !puzzle_string.match(/[^0-9]+/).nil?
-    
-    puzzle_string
+    raise StringHasInvalidCharacters unless puzzle_string.match(/[^0-9]+/).nil?
+
+    generate_grid(puzzle_string)
+  end
+
+  def generate_grid(sudoku_str)
+    # splits string into array of lines, each line contains nine chars
+    lines = sudoku_str.scan(/.{9}/)
+
+    lines.each do |line|
+      line.insert(6, '|')
+      line.insert(3, '|')
+      line.insert(-1, '-')
+      line.insert(0, '-')
+      # i hate this while loop, i have no idea how to do it any other way though.
+      i = 0
+      while i < line.length
+        line.insert(i, ' ') if i.odd?
+        i += 1
+      end
+    end
+
+    lines.insert(6, '-------------------------')
+    lines.insert(3, '-------------------------')
+    lines.insert(-1, '-------------------------')
+    lines.insert(0, '-------------------------')
+    lines.join("\n")
   end
 end
-
--- src/test/test_solver.rb
+++ src/test/test_solver.rb
@@ -16,56 +16,64 @@ require 'rspec/autorun'
 require_relative '../classes/ParseSudoku'
 
 describe ParseSudoku do
-  let (:puzzle) {ParseSudoku.new}
+  let(:puzzle) { ParseSudoku.new }
 
-  describe '#parse_sudoku' do 
-
+  describe '#parse_sudoku' do
     context 'when input not a string, ' do
-
       it 'recieves no input.' do
-        expect {puzzle.parse_sudoku}.to raise_error(ArgumentError)
+        expect { puzzle.parse_sudoku }.to raise_error(ArgumentError)
       end
       it 'input is nil.' do
-        expect{puzzle.parse_sudoku(nil)}.to raise_error(ParseSudoku::NotStringError)
+        expect { puzzle.parse_sudoku(nil) }.to raise_error(ParseSudoku::NotStringError)
       end
       it 'input is integer.' do
-        expect{puzzle.parse_sudoku(123)}.to raise_error(ParseSudoku::NotStringError)
+        expect { puzzle.parse_sudoku(123) }.to raise_error(ParseSudoku::NotStringError)
       end
       it 'input is float.' do
-        expect{puzzle.parse_sudoku(12.3)}.to raise_error(ParseSudoku::NotStringError)
+        expect { puzzle.parse_sudoku(12.3) }.to raise_error(ParseSudoku::NotStringError)
       end
       it 'input is boolean, true.' do
-        expect{puzzle.parse_sudoku(true)}.to raise_error(ParseSudoku::NotStringError)
+        expect { puzzle.parse_sudoku(true) }.to raise_error(ParseSudoku::NotStringError)
       end
       it 'input is boolean, false.' do
-        expect{puzzle.parse_sudoku(false)}.to raise_error(ParseSudoku::NotStringError)
+        expect { puzzle.parse_sudoku(false) }.to raise_error(ParseSudoku::NotStringError)
       end
-      
     end
-    
-    
-    context 'when passed an invalid string, 'do
-      it 'longer than 81 characters.'do
-        expect{puzzle.parse_sudoku('123456789123456789123456789123456789123456789123456789123456789123456789123456789000')}
+
+    context 'when passed an invalid string, ' do
+      it 'longer than 81 characters.' do
+        expect { puzzle.parse_sudoku('123456789123456789123456789123456789123456789123456789123456789123456789123456789000') }
           .to raise_error(ParseSudoku::StringTooLong)
       end
-      it 'shorter than 81 characters.'do
-        expect{puzzle.parse_sudoku('123456789123456789123456789123456789123456789123456789123456789123456789123456')}
+      it 'shorter than 81 characters.' do
+        expect { puzzle.parse_sudoku('123456789123456789123456789123456789123456789123456789123456789123456789123456') }
           .to raise_error(ParseSudoku::StringTooShort)
       end
       it 'contains non-numeric characters.' do
-        expect{puzzle.parse_sudoku('wrong6789123456789123456789123456789123456789123456789123456789123456789123456789')}
-        .to raise_error(ParseSudoku::StringHasInvalidCharacters)
+        expect { puzzle.parse_sudoku('wrong6789123456789123456789123456789123456789123456789123456789123456789123456789') }
+          .to raise_error(ParseSudoku::StringHasInvalidCharacters)
       end
     end
 
-    context 'when passed a valid string' do
-      it 'string is valid' do
-        expect(puzzle.parse_sudoku("123456789123456789123456789123456789123456789123456789123456789123456789123456789"))
-        .to eql("123456789123456789123456789123456789123456789123456789123456789123456789123456789")
+    context 'when passed a valid string,' do
+      it 'returns valid.' do
+        expect(puzzle.parse_sudoku('123456789123456789123456789123456789123456789123456789123456789123456789123456789'))
+          .to eq(
+            "-------------------------\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "-------------------------\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "-------------------------\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            "- 1 2 3 | 4 5 6 | 7 8 9 -\n" \
+            '-------------------------'
+          )
       end
     end
   end
-
-
 end
